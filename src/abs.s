e4e069f4962a5647230c6ea4777afa7867c9bc88
404allen404 <n26120935@gs.ncku.edu.tw>
2024-11-09 06:05:46 +0800

    Implement argmax.s

--- src/abs.s
+++ src/abs.s
@@ -17,9 +17,9 @@
 abs:
     lw t0 0(a0)        # load number from memory
     bge t0, zero, done
-    xori t0, t0, -1
-    addi t0, t0, 1
-    sw t0, 0(a0)
+    xori t0, t0, -1    # t0 = ~t0
+    addi t0, t0, 1     # t0 = t0 + 1
+    sw t0, 0(a0) 
 
 done:
     jr ra
--- src/argmax.s
+++ src/argmax.s
@@ -25,12 +25,30 @@ argmax:
     li t6, 1
     blt a1, t6, handle_error
 
-    lw t0, 0(a0)
-
-    li t1, 0
-    li t2, 1
+    lui t1, 0x80000 # t1 = max
+    addi t3, a1, -1 # t3 = i = n - 1
+    add t4, x0, a0  # t4 = base address
+    add a0, x0, x0  # a0 = idx = 0
+    
 loop_start:
-    # TODO: Add your own implementation
+    slli t5, t3, 2               # t5 = i * 4
+    add t5, t5, t4
+    lw t0, 0(t5)                 # t0 = element
+    blt t0, t1, if_continue_loop
+
+change_max:
+    add t1, x0, t0
+    add a0, x0, t3
+
+if_continue_loop:
+    addi t3, t3, -1
+    blt t3, x0, done
+
+jump_to_loop_start:
+    jal x0, loop_start
+
+done:
+    jr ra
 
 handle_error:
     li a0, 36
